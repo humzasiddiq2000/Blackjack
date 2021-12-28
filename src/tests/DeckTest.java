4d115c53cbd01f6e276d51920029b9b36dfad94d
humzasiddiq2000 <humza.siddiq.2017@uni.strath.ac.uk>
2021-12-28 15:49:59 +0000

    ADD deck - number of cards + populated Tests

---
+++ src/tests/DeckTest.java
@@ -0,0 +1,38 @@
+package tests;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import main.*;
+
+/**
+ * Deck class tests
+ * 
+ * @author Humza Siddiq
+ */
+public class DeckTest {
+
+    Deck deck;
+
+    @Before
+    public void setUp(){
+        deck = new Deck();
+    }
+    
+    @Test
+    public void numberOfCards(){
+        assertTrue(deck.getDeck().length == 52);
+    }
+
+    @Test
+    public void checkDeckPopulated(){
+        Card[] cardDeck = deck.getDeck();
+        for(int i = 0; i < cardDeck.length; i++){
+            assertTrue(cardDeck[i] != null);
+        }
+    }
+}
