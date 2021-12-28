3d107db2ff5ac4f61898f4b6fa11a55eedd38e2e
humzasiddiq2000 <humza.siddiq.2017@uni.strath.ac.uk>
2021-12-28 14:57:08 +0000

    ADD Deck class

---
+++ src/Deck.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Deck class represents a card deck.
+ * Containing deck operations.
+ * 
+ * @author Humza Siddiq
+ */
+public class Deck {
+
+    // 52 cards in a (standard) deck
+    private Card[] deck = new Card[52];
+    private int nextCardPointer = 0;
+
+    public Deck(){
+
+    }
+
+
+}
