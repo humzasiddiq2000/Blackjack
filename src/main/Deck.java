1f5ffd185fadedba1a5e93126bf3c342fcd3aa2c
humzasiddiq2000 <humza.siddiq.2017@uni.strath.ac.uk>
2021-12-28 15:48:45 +0000

    ADD populate deck

---
+++ src/main/Deck.java
@@ -0,0 +1,79 @@
+package main;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Deck class represents a card deck.
+ * Containing deck operations.
+ * 
+ * @author Humza Siddiq
+ */
+public class Deck {
+
+    private Card[] deck = new Card[52]; // 52 cards in a (standard) deck
+    private int nextCardPointer = 0;
+    private Map<String, Integer> cardValues = new HashMap<>();
+    private Set<String> cardSuits = new HashSet<>();
+
+    public Deck(){
+        initialiseValueMapping();
+        initaliseSuitMapping();
+        populateDeck();
+    }
+
+    private void populateDeck() {
+        String[] cardValuesArray = (String[]) cardValues.keySet().toArray(new String[0]);
+        String[] cardSuitsArray  = (String[]) cardSuits.toArray(new String[0]);
+        int deckIndex = 0;
+
+        for(int suitIndex = 0; suitIndex < cardSuitsArray.length; suitIndex++){
+            for(int valueIndex = 0; valueIndex < cardValuesArray.length; valueIndex++){
+                String suit = cardSuitsArray[suitIndex];
+                String value = cardValuesArray[valueIndex];
+
+                Card playingCard = new Card(suit, value);
+                deck[deckIndex] = playingCard;
+                deckIndex++;
+            }
+        }
+    }
+
+    private void initaliseSuitMapping() {
+        cardSuits.add("clubs");
+        cardSuits.add("diamonds");
+        cardSuits.add("hearts");
+        cardSuits.add("spades");
+    }
+
+    private void initialiseValueMapping() {
+        cardValues.put("ace", 1);
+        cardValues.put("2", 2);
+        cardValues.put("3", 3);
+        cardValues.put("4", 4);
+        cardValues.put("5", 5);
+        cardValues.put("6", 6);
+        cardValues.put("7", 7);
+        cardValues.put("8", 8);
+        cardValues.put("9", 9);
+        cardValues.put("10", 10);
+        cardValues.put("jack", 10);
+        cardValues.put("queen", 10);
+        cardValues.put("king", 10);
+    }
+
+    public Card[] getDeck(){
+        return deck;
+    }
+
+    public String toString(){
+        return Arrays.toString(deck);
+    }
+
+    public static void main(String args[]){
+        Deck d = new Deck();
+        System.out.println(d.toString());
+    }
+}
